66e7ffde2a382e34a0acd49a7c431d2fd92ed624
mahadidev <mahadi.dev.pm@gmail.com>
2023-06-09 10:35:12 +0600

    update title and remove vercel config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import type { AppProps } from "next/app";
 import { ContextProvider } from "../context";
 import "./globals.css";
+import Head from "next/head";
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <ContextProvider>
+      <Head>
+        <title>AI Chat Assistant for Voice and Text</title>
+      </Head>
       <Component {...pageProps} />
     </ContextProvider>
   );
