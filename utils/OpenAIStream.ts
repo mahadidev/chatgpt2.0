dfc2b6fd7ded65301d340bdb86d4aa034dc40d2b
mahadidev <mahadi.dev.pm@gmail.com>
2023-06-09 11:36:01 +0600

    fix vercel

--- utils/OpenAIStream.ts
+++ utils/OpenAIStream.ts
@@ -21,7 +21,7 @@ export async function OpenAIStream(payload: OpenAIStreamPayload) {
 
   const requestHeaders: Record<string, string> = {
     "Content-Type": "application/json",
-    Authorization: `Bearer ${process.env.OPENAI_API_KEY ?? ""}`,
+    Authorization: `Bearer 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`,
   };
 
   if (process.env.OPENAI_API_ORG) {
